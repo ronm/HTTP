d65a2a86aed96a9fef5b13fbbf7ab0cf4cc7060b
Ron <ron@Linkcreative.com>
2017-06-15 22:59:36 -0700

    Redoing API adding readme

--- HTTP.js
+++ HTTP.js
@@ -10,87 +10,65 @@
         root.Http = factory();
   }
 }(this, function () {
-	class Http {
-		constructor(url, options = {}) {
-			this._headers = {};
-			this._data = {};
+	return class Http {
+		constructor(options = {}) {
+			// store headers
+			this.headers = {};
+			this._addHeaders(options.headers);
+			
+			// create client
 			this.client = new XMLHttpRequest();
 		}
 	
-		request(method, data) {
-			let isData = Object.keys(this.data()).length, 
-				headers = Object.keys(this._headers);
-				
-			this.url = url + (isData && ["get","delete"].indexOf(method)>-1 ? ("?" + this._formatData()) : "");
-
-			this.client.open(method, this.url);
+		request(method, url, data) {
 			
-			if ( headers.length ) {
-				headers.forEach(key => this.client.setRequestHeader(key, this._headers[key]));
-			}
+			//open url
+			this.client.open(method, url + (data && ["get","delete"].indexOf(method)>-1 ? ("?" + this._formatData(data)) : ""));
 			
-			if ( isData && !this._headers["Content-type"]) {
+			// headers
+			Object.keys(this.headers).forEach(key => this.client.setRequestHeader(key, this.headers[key]));
+			if ( data && !this.headers["Content-type"]) {
 				this.client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			}			
-					
-			return new Promise((good, bad) => {
+			}
+
+			// promise
+			return new Promise((good, bad) => { 
+				// client bindings
 				this.client.addEventListener("load", () => (this._is200()) ? good(this.client.response, this.client) : bad(this.client));
-				this.client.addEventListener("error", () => bad(this.client));	
-				this.client.send((isData && ["post","put"].indexOf(method)>-1)?this._formatData():null);
-			});	
+				this.client.addEventListener("error", () => bad(this.client));				
+				
+				// send request
+				this.client.send((data && ["post","put"].indexOf(method)>-1)?this._formatData(data):null)
+			});
 		}
 
 
 		/*******************
 		*** ajax methods ***
 		*******************/		
-		get(url,data,options) { return this.request("get",url,data,options); }
-		post(url,data,options) { return this.request("post",url,data,options); }
-		put(url,data,options) { return this.request("put",url,data,options); }
-		delete(url,data,options) { return this.request("delete",url,data,options); }
-	
-	
-		/**************
-		*** helpers ***
-		**************/
-		headers(header = null) {
-			if ( header ) {
-				Object.keys(header).forEach(k => this._headers[k] = header[k]);
-				return this;
-			} else {
-				return this._headers;
-			}
-		}
-		
-		data(data) {
-			if ( data ) {
-				Object.keys(data).forEach(k => this._data[encodeURI(k)] = encodeURI(data[k]));
-				return this;
-			} else {
-				return this._data;
-			}			
-		}
+		get(url,data) { return this.request("get",url,data); }
+		post(url,data) { return this.request("post",url,data); }
+		put(url,data) { return this.request("put",url,data); }
+		delete(url,data) { return this.request("delete",url,data); }
 		
 		
 		/**********************
 		*** private helpers ***
 		**********************/
-		_formatData() {
-			return Object.keys(this._data).map(key => key + "=" + this._data[key]).join("&");
+		_formatData(data = {}) {
+			return Object.keys(data).map(key => key + "=" + data[key]).join("&");
 		}
-	
-		_addHeaders(headers) {
-			headers.forEach(h => {
-				let k = Object.keys(h)[0];
-				this.client.setRequestHeader(k, h[k]);
-			});
-		}
+		
+		_addHeaders(headers = {}) {
+			Object.keys(headers).forEach(k => this.headers[k] = headers[k]);
+		}		
 		
 		_is200() {
 			return this.client.status >= 200 && this.client.status < 300;
-		}	
-	}
-	
-	return function() { return new Http(); }
-	
+		}
+		
+		// *** Options
+		// add form headers by deafult for post
+		// parseJSON
+	}	
 }));
---
+++ README.md
@@ -0,0 +1,75 @@
+Http
+=========
+
+XMLHttpRequest Object
+
+Usage
+------
+
+```js
+import Http from './Http'
+```
+
+or
+
+```html
+<script type="text/javascript" src="http.js"></script>
+```
+
+then
+
+```js
+var http = new Http();
+
+http.get(url = 'string', data = {}).then(res => console.log(res));
+```
+
+
+Methods
+-------
+
+#### get
+
+```js
+new Http().get(url,data);
+```
+
+
+#### post
+
+```js
+new Http().post(url,data);
+```
+
+
+#### delete
+
+```js
+new Http().delete(url,data);
+```
+
+
+#### put
+
+```js
+new Http().put(url,data);;
+```
+
+
+
+Options
+-------
+
+Options are set when initializing the Http object.
+
+
+#### headers: null (optional parameter)
+
+`object`
+
+
+```js
+var http = new Http({headers: {"Content-type": "application/x-www-form-urlencoded"}});
+
+http.get(url = 'string', data = {})	.then(res => console.log(res));
+```
